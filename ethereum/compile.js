e3ca937d4e32b766eba9b42f79df5844dfd60b4d
shelinals97 <shelinals97@gmail.com>
2018-09-25 14:42:56 +0800

    add compile and deploy

---
+++ ethereum/compile.js
@@ -0,0 +1,27 @@
+/* 
+    runtime:
+    delete entire 'build' folder
+    read 'campaign.sol' from the 'contracts' folder
+    compile both contracts with solidity compiler
+    write output to the 'build' directory
+*/
+
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra'); //improved version of fs module
+
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+const campaignPath = path.resolve(__dirname, 'contracts', 'Rental.sol');
+const source = fs.readFileSync(campaignPath, 'utf8');
+const output = solc.compile(source, 1).contracts;
+
+fs.ensureDirSync(buildPath);    //ensure directory exist, if not create one
+
+for(let contract in output) {
+    fs.outputJsonSync(          //write json file (path, data)
+        path.resolve(buildPath, contract.replace(':', '') + '.json'),
+        output[contract]
+    );
+}
---
+++ ethereum/deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/RentalFactory.json');
+
+const provider = new HDWalletProvider(
+    'rally three arrest upper zone flat family water enact napkin stand only',
+    'https://rinkeby.infura.io/v3/d928a9c204db4d418b536b7dbcf89977'
+); //mneumonic can specify many accounts
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+        .deploy({ data: '0x' + compiledFactory.bytecode })
+        .send({ from: accounts[0], gas: '1000000', gasPrice: web3.utils.toWei('2', 'gwei') });
+
+    console.log('Contract deployed to ', result.options.address);
+    //keep the address for now!
+};
+deploy();
